7b87f4ea54979426a82b667417bd21d1405e1144
Jacqueline <jacquelineomollo@gmail.com>
2019-06-09 14:19:35 -0400

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-// Your code goes here
+// header color change
+let mainHeader = document.querySelector("header");
+mainHeader.style.backgroundColor = "seashell";
+
+//logo mouseover
+let logo = document.querySelector(".logo-heading");
+logo.addEventListener(
+  "mouseover",
+  event => {
+    event.target.style.color = "orange";
+
+    setTimeout(function() {
+      event.target.style.color = "black";
+    }, 500);
+  },
+  false
+);
+
+//nav links mouseover
+let mainNav = document.querySelectorAll(" a ");
+for (let i = 0; i < mainNav.length; ++i) {
+  mainNav[i].addEventListener(
+    "mouseover",
+    event => {
+      event.target.style.color = "orange";
+
+      setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+    },
+    false
+  );
+}
+
+let container = document.querySelectorAll(".container .home");
+container.addEventListener("scroll", function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    container.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+let containerHome = doucument.querSelector(".intro img");
+
+let letsgo = document.querySelectorAll("");
